ea1bfdce75c3b6a0a331418a67ec3762f8c69cd2
Erick Llerenas <dev.llerenas@gmail.com>
2024-02-13 18:50:29 -0600

    refactor code

--- build/index.js
+++ build/index.js
@@ -33,5 +33,4 @@ io.on("connection", (socket) => {
         console.log("Un cliente se ha desconectado");
     });
 });
-const port = parseInt(process.env.PORT || "3000", 10);
-server.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
