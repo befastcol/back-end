d2db7d44a77e0335c80298a282a5799dfd2baa9e
Erick Llerenas <dev.llerenas@gmail.com>
2024-02-13 18:31:08 -0600

    refactor

--- index.ts
+++ index.ts
@@ -39,6 +39,6 @@ io.on("connection", (socket) => {
 
 export { io };
 
-const port: number = parseInt(process.env.PORT || "3000", 10);
-
-server.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running on port ${process.env.PORT || 3000}`)
+);
