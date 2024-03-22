46378c4af6395de14dbed879f762d011cc05dba8
Erick Llerenas <dev.llerenas@gmail.com>
2024-03-21 18:14:11 -0600

    remove pagination

--- src/controllers/deliveries.ts
+++ src/controllers/deliveries.ts
@@ -112,17 +112,10 @@ export const updateDelivery = async (req: Request, res: Response) => {
 
 export const getCourierDeliveries = async (req: Request, res: Response) => {
   try {
-    const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 25;
-    const skip = (page - 1) * limit;
-
     const courierId = req.params.courierId;
-    const courierServices = await Delivery.find({ courier: courierId })
-      .sort({
-        requestedDate: -1,
-      })
-      .skip(skip)
-      .limit(limit);
+    const courierServices = await Delivery.find({ courier: courierId }).sort({
+      requestedDate: -1,
+    });
 
     res.status(200).json(courierServices);
   } catch (_) {
