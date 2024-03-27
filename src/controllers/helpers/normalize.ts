d481031b8286a4abf608724d403699ac48c3c564
Erick Llerenas <dev.llerenas@gmail.com>
2024-03-27 16:54:55 -0600

    normalize originLocation when updated

--- src/controllers/helpers/normalize.ts
+++ src/controllers/helpers/normalize.ts
@@ -1,7 +1,11 @@
 export const normalizeCityName = (cityName: string) => {
+  // Paso 1: Convertir a minúsculas
   let normalized = cityName.toLowerCase();
 
+  // Paso 2: Eliminar prefijos comunes como "ciudad de"
   normalized = normalized.replace(/^ciudad de\s+/, "");
+
+  normalized = normalized.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
   return normalized;
 };
